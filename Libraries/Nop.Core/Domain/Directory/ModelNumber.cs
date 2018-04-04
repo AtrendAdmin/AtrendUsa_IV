db5641a9c3140b23498b258a08e15653947484df
nikolay@sparkequation.com <nikolay@sparkequation.com>
2018-04-04 06:43:54 -0500

    Additional fixes

---
+++ Libraries/Nop.Core/Domain/Directory/ModelNumber.cs
@@ -0,0 +1,21 @@
+﻿using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Core.Domain.Directory
+{
+    /// <summary>
+    /// Represents a ModelNumber     //Added by IV Santosh 
+    /// </summary>
+    public partial class ModelNumber : BaseEntity, ILocalizedEntity
+    {
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string ModelNum { get; set; }
+    }
+}
